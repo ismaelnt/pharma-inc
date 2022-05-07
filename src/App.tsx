baab9b960e210649bd99b1c3fbf2457cd7e08894
ismaelnt <ismaelnt97@gmail.com>
2022-05-07 15:55:38 -0300

    feat: component header

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { Header } from './components/Header';
 
 export function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <div>
+        <Header />
+        <h1>hello</h1>
+      </div>
+    </>
   );
 }
---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+
+export function Header() {
+  return (
+    <div className='bg-gray-800'>
+      <div className='flex items-center justify-between max-w-7xl mx-auto py-6 px-6'>
+        <p className='text-3xl'>Pharma Inc</p>
+        <div className="m-1 mr-2 w-12 h-12 relative flex justify-center items-center rounded-full bg-slate-500 text-xl text-white uppercase">
+          IN
+        </div>
+      </div>
+    </div>
+  );
+}
